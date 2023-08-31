47a91077ebd7127487292c72c421ad9702716ecc
InitSync <benjaalejandro206@gmail.com>
2023-08-31 12:23:48 -0400

    chore(ProjectileEvents): improve structure in all methods, do use of 'config' field instead of do method calls, store values that are used two times or more, and use a Set object instead of an ArrayList

--- src/main/java/hacky/dev/events/ProjectileEvents.java
+++ src/main/java/hacky/dev/events/ProjectileEvents.java
@@ -3,10 +3,7 @@ package hacky.dev.events;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Egg;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
-import org.bukkit.entity.Snowball;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -14,84 +11,85 @@ import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import hacky.dev.MNHP;
 import hacky.dev.handlers.PathwayHandler;
+import org.bukkit.projectiles.ProjectileSource;
 
-import java.util.ArrayList;
-import java.util.UUID;
+import java.util.*;
 
 public class ProjectileEvents implements Listener {
-    private ArrayList<UUID> thrownEggs = new ArrayList<>();
-    private FileConfiguration config;
-
-    public ProjectileEvents(FileConfiguration config) {
-        this.config = config;
-    }
-
-    @EventHandler
-    public void onThrow(PlayerInteractEvent event) {
-        if (event.getPlayer().getItemInHand() != null) {
-            ItemStack stack = event.getPlayer().getItemInHand();
-            if (stack.getType().equals(Material.EGG) || stack.getType().equals(Material.SNOW_BALL)) {
-                if (event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-                    if (stack.hasItemMeta()) {
-                        if (stack.getItemMeta().hasDisplayName()) {
-                            String eggName = ChatColor.translateAlternateColorCodes('&',
-                                    MNHP.getInstance().getPluginConfig().getString("items.eggName"));
-                            String ballName = ChatColor.translateAlternateColorCodes('&',
-                                    MNHP.getInstance().getPluginConfig().getString("items.ballName"));
-
-                            if (stack.getItemMeta().getDisplayName().equals(eggName) ||
-                                    stack.getItemMeta().getDisplayName().equals(ballName)) {
-                                thrownEggs.add(event.getPlayer().getUniqueId());
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    @EventHandler
-    public void onHit(ProjectileHitEvent event) {
-        if (event.getEntity() instanceof Egg || event.getEntity() instanceof Snowball) {
-            if (event.getEntity() instanceof Egg) {
-                Egg egg = (Egg) event.getEntity();
-                if (PathwayHandler.pathways.containsKey(egg)) {
-                    PathwayHandler.pathways.remove(egg);
-                }
-            } else {
-                Snowball ball = (Snowball) event.getEntity();
-                if (PathwayHandler.pathways.containsKey(ball)) {
-                    PathwayHandler.pathways.remove(ball);
-                }
-            }
-        }
-    }
-
-    @EventHandler
-    public void onLaunch(ProjectileLaunchEvent event) {
-        if (event.getEntity() instanceof Egg || event.getEntity() instanceof Snowball) {
-            Player player;
-            if (event.getEntity() instanceof Egg) {
-                Egg egg = (Egg) event.getEntity();
-                if (egg.getShooter() instanceof Player) {
-                    player = (Player) egg.getShooter();
-                    if (thrownEggs.contains(player.getUniqueId())) {
-                        thrownEggs.remove(player.getUniqueId());
-                        PathwayHandler.pathways.put(egg, Material.WOOL);
-                    }
-                }
-            } else {
-                Snowball ball = (Snowball) event.getEntity();
-                if (ball.getShooter() instanceof Player) {
-                    player = (Player) ball.getShooter();
-                    if (thrownEggs.contains(player.getUniqueId())) {
-                        thrownEggs.remove(player.getUniqueId());
-                        PathwayHandler.pathways.put(ball, Material.WOOL);
-                    }
-                }
-            }
-        }
-    }
+	private final Set<UUID> thrownEggs;
+	private final FileConfiguration config;
+	private final PathwayHandler pathwayHandler;
+	
+	public ProjectileEvents(final FileConfiguration config, final PathwayHandler pathwayHandler) {
+		thrownEggs = new HashSet<>();
+		this.config = Objects.requireNonNull(config, "FileConfiguration reference cannot be null.");
+		this.pathwayHandler = Objects.requireNonNull(pathwayHandler, "PathwayHandler reference cannot be null.");
+	}
+	
+	@EventHandler
+	public void onThrow(final PlayerInteractEvent event) {
+		final Player player = event.getPlayer();
+		final ItemStack item = event.getPlayer().getItemInHand();
+		// Checks if the item in the player hand is null.
+		if (item == null) {
+			return;
+		}
+		final Material type = item.getType();
+		// Checks if the type is an Egg or a Snow Ball.
+		if ((type != Material.EGG) && (type != Material.SNOW_BALL)) {
+			return;
+		}
+		// Checks if the action is distinct to a 'RIGHT_CLICK_AIR' type.
+		if (event.getAction() != Action.RIGHT_CLICK_AIR) {
+			return;
+		}
+		final String displayName = item.getItemMeta().getDisplayName();
+		// Checks the content of the item display name.
+		if (displayName.equals(ChatColor.translateAlternateColorCodes('&', config.getString("items.eggName"))) ||
+			displayName.equals(ChatColor.translateAlternateColorCodes('&', config.getString("items.ballName")))
+		) {
+			thrownEggs.add(player.getUniqueId());
+		}
+	}
+	
+	@EventHandler
+	public void onHit(final ProjectileHitEvent event) {
+		final List<Projectile> projectiles = pathwayHandler.projectiles();
+		final Projectile projectile = event.getEntity();
+		final EntityType type = projectile.getType();
+		// Checks if the entity type is a 'EGG'.
+		// Else, check if the type is a 'SNOWBALL'.
+		if ((type != EntityType.EGG) && (type != EntityType.SNOWBALL)) {
+			return;
+		}
+		// Checks if the projectiles list contains that reference.
+		if (!projectiles.contains(projectile)) {
+			return;
+		}
+		projectiles.remove(projectile);
+	}
+	
+	@EventHandler
+	public void onLaunch(final ProjectileLaunchEvent event) {
+		final Projectile projectile = event.getEntity();
+		final EntityType type = projectile.getType();
+		// Checks if the entity type is a 'EGG'.
+		// Else, check if the type is a 'SNOWBALL'.
+		if ((type != EntityType.EGG) && (type != EntityType.SNOWBALL)) {
+			return;
+		}
+		final ProjectileSource shooter = projectile.getShooter();
+		// Checks if the shooter is a player.
+		if (!(shooter instanceof Player)) {
+			return;
+		}
+		final UUID id = ((Player) shooter).getUniqueId();
+		// Checks if the set contains this id.
+		if (!thrownEggs.contains(id)) {
+			return;
+		}
+		thrownEggs.remove(id);
+		pathwayHandler.projectiles().add(projectile);
+	}
 }
