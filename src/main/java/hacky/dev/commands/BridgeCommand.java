674ff228e186caecf63266dec748b9d4d3941422
Ricardo Soto <75344350+ZxyretrohackyxZ@users.noreply.github.com>
2023-08-31 00:54:59 -0600

    Update BridgeCommand.java

--- src/main/java/hacky/dev/commands/BridgeCommand.java
+++ src/main/java/hacky/dev/commands/BridgeCommand.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -12,48 +13,98 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import hacky.dev.handlers.PathwayHandler;
 
-public class BridgeCommand implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeCommand implements CommandExecutor, TabCompleter {
     private final JavaPlugin plugin;
     private final PathwayHandler pathwayHandler;
 
     public BridgeCommand(JavaPlugin plugin, PathwayHandler pathwayHandler) {
         this.plugin = plugin;
         this.pathwayHandler = pathwayHandler;
+        plugin.getCommand("bridge").setExecutor(this);
+        plugin.getCommand("bridge").setTabCompleter(this);
     }
 
-    public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] args) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage(getMessage("noPermission"));
             return false;
-        } else {
-            Player pl = (Player) sender;
-            if (!pl.hasPermission("bridge.command")) {
-                pl.sendMessage(getMessage("noPermission"));
-                return false;
-            } else if (args.length >= 1) {
-                if (args[0].equalsIgnoreCase("egg")) {
-                    pl.getInventory().addItem(new ItemStack[]{this.getItem("EGG")});
-                    pl.sendMessage(getMessage("bridgeEggGiven"));
-                    return true;
-                } else if (args[0].equalsIgnoreCase("ball")) {
-                    pl.getInventory().addItem(new ItemStack[]{this.getItem("BALL")});
-                    pl.sendMessage(getMessage("bridgeBallGiven"));
-                    return true;
-                } else {
-                    pl.sendMessage(getMessage("usage"));
+        }
+
+        Player pl = (Player) sender;
+
+        if (!pl.hasPermission("bridge.command")) {
+            pl.sendMessage(getMessage("noPermission"));
+            return false;
+        }
+
+        if (args.length == 0) {
+            plugin.reloadConfig();
+            pl.sendMessage(getMessage("configReloaded"));
+            return true;
+        }
+
+        switch (args[0].toLowerCase()) {
+            case "give":
+                if (args.length < 2) {
+                    pl.sendMessage(getMessage("giveUsage"));
                     return false;
                 }
-            } else {
+
+                switch (args[1].toLowerCase()) {
+                    case "egg":
+                        pl.getInventory().addItem(new ItemStack[]{this.getItem("EGG")});
+                        pl.sendMessage(getMessage("bridgeEggGiven"));
+                        return true;
+                    case "ball":
+                        pl.getInventory().addItem(new ItemStack[]{this.getItem("BALL")});
+                        pl.sendMessage(getMessage("bridgeBallGiven"));
+                        return true;
+                    default:
+                        pl.sendMessage(getMessage("giveUsage"));
+                        return false;
+                }
+            case "reload":
+                plugin.getServer().getPluginManager().disablePlugin(plugin);
+                plugin.getServer().getPluginManager().enablePlugin(plugin);
+                pl.sendMessage(getMessage("pluginReloaded"));
+                return true;
+            default:
                 pl.sendMessage(getMessage("usage"));
                 return false;
-            }
         }
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
+        if (args.length == 1) {
+            List<String> subCommands = new ArrayList<>();
+            subCommands.add("give");
+            subCommands.add("reload");
+            return subCommands;
+        } else if (args.length == 2 && args[0].equalsIgnoreCase("give")) {
+            List<String> giveSubCommands = new ArrayList<>();
+            giveSubCommands.add("egg");
+            giveSubCommands.add("ball");
+            return giveSubCommands;
+        }
+        return null;
     }
 
     private String getMessage(String key) {
         FileConfiguration config = plugin.getConfig();
-        return ChatColor.translateAlternateColorCodes('&', config.getString("messages." + key));
+        String message = config.getString("messages." + key);
+
+        if (message != null) {
+            return ChatColor.translateAlternateColorCodes('&', message);
+        } else {
+            return "Message not found in config";
+        }
     }
+
 
     private ItemStack getItem(String type) {
         ItemStack stack = new ItemStack(this.getType(type));
