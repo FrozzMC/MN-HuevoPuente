8cf64ee28551873462d71e3384e143424012bf78
InitSync <benjaalejandro206@gmail.com>
2023-08-31 12:19:49 -0400

    chore(MNHP): use onLoad() method for instances creation and config load process, and use onDisable() method for the plugin tasks cancellation

--- src/main/java/hacky/dev/MNHP.java
+++ src/main/java/hacky/dev/MNHP.java
@@ -1,5 +1,6 @@
 package hacky.dev;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import hacky.dev.commands.BridgeCommand;
@@ -7,37 +8,35 @@ import hacky.dev.events.ProjectileEvents;
 import hacky.dev.handlers.PathwayHandler;
 
 public class MNHP extends JavaPlugin {
-    private static MNHP instance;
-    private FileConfiguration config;
-    private PathwayHandler pathwayHandler;
-
-    @Override
-    public void onEnable() {
-        instance = this;
-
-        config = getConfig();
-        config.options().copyDefaults(true);
-        saveDefaultConfig();
-
-        pathwayHandler = new PathwayHandler();
-        getServer().getPluginManager().registerEvents(new ProjectileEvents(config), this);
-
-        new BridgeCommand(this, pathwayHandler);
-    }
-
-    @Override
-    public void onDisable() {
-    }
-
-    public static MNHP getInstance() {
-        return instance;
-    }
-
-    public FileConfiguration getPluginConfig() {
-        return config;
-    }
-
-    public PathwayHandler getPathwayHandler() {
-        return pathwayHandler;
-    }
+	private static MNHP instance;
+	
+	private FileConfiguration config;
+	private PathwayHandler pathwayHandler;
+	
+	@Override
+	public void onLoad() {
+		instance = this;
+		
+		config = getConfig();
+		config.options().copyDefaults(true);
+		saveDefaultConfig();
+		
+		pathwayHandler = new PathwayHandler();
+	}
+	
+	@Override
+	public void onEnable() {
+		pathwayHandler.startCheckTask();
+		getServer().getPluginManager().registerEvents(new ProjectileEvents(config, pathwayHandler), this);
+		new BridgeCommand(this, config);
+	}
+	
+	@Override
+	public void onDisable() {
+		Bukkit.getScheduler().cancelTasks(this);
+	}
+	
+	public static MNHP getInstance() {
+		return instance;
+	}
 }
